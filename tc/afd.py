fef19b35df9189bf8ce5a546b82a9bbd5edbb4c1
joeldossantos <jsantos@eic.cefet-rj.br>
2021-02-24 11:07:30 -0300

    Ajustes no PR

--- tc/afd.py
+++ tc/afd.py
@@ -23,10 +23,13 @@ def aceita(automato, palavra):
 
 def computacao(automato, estado, palavra):
     if palavra == []:
-        estados_subpalavras.append([estado, palavra.copy()])
-        return " |- ".join(["(" + str(estado_subpalavra[0]) + ", " + "".join(str(subpalavra) for subpalavra in estado_subpalavra[1]) + ")" for estado_subpalavra in estados_subpalavras])
+        return "(" + estado + ", )"
     else:
-        estados_subpalavras.append([estado, palavra.copy()])
-        subpalavra = palavra.pop(0)
-        estadoAtual = delta_hat(automato, estado, [subpalavra])
-        return computacao(automato, estadoAtual, palavra)
+        config = "(" + estado + ", " + printw(palavra) + ") |- "
+        simbolo = palavra.pop(0)
+        proximo = delta(automato, estado, simbolo)
+        return config + computacao(automato, proximo, palavra)
+
+
+def printw(palavra):
+    return "".join(str(simbolo) for simbolo in palavra)
