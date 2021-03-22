f2c8ccb22afb94f1700ca3b354a55b915bcc5179
Matheus Melo <73976198+matheussmelo@users.noreply.github.com>
2021-03-22 16:32:59 -0300

    Update afn.py
    Exemplo de autômato alocado para a pasta Exemplos. ✅

--- tc/afn.py
+++ tc/afn.py
@@ -17,24 +17,3 @@ def eclose(automato,estado):
         eclosure = eclosure.union(delta(automato, estados, simbolo))  # Nesta linha o eclosure está utilizando o .union para somar o eclose de cada elemento do set de entrada e guardar na própria variável.
     return eclosure # Será retornado um Set com o resultado da soma do eclose de cada Set de entrada.
 
-# Pode-se utilizar o dicionário abaixo como exemplo (Slide 16 de AFN)
-"""
-Sigma = {'x', 'y'}
-QSet = {'a', 'b', 'c','d','e','f','g'}
-ESet = {'c','d','g'} # ESet não faz parte do exemplo do slide, é apenas um Set criado para utilizar como exemplo de entrada.
-FSet = {'g'}
-Delta = {
-            ('a',''): {'a','b','c','d','e'},
-            ('b',''): {'b','c','d'},
-            ('c',''): {'c','d'},
-            ('d',''): {'d'},
-            ('e',''): {'e'},
-            ('e','x'): {'f'},
-            ('f',''): {'f','g'},
-            ('f','y'): {'d'},
-            ('g',''): {'g'},
- }
-
-M1 = (QSet, Sigma, Delta, 'q1', FSet)
-"""
-# print(eclose(M1,ESet))
