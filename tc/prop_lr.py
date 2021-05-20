63aff9cedaf329ac779e7e6e2c91a17174fd1e17
LewiksR <luik98@gmail.com>
2021-05-19 23:02:25 -0300

    Refatoração dos itens 21, 22 e 23 para usar um dicionario ao inves de uma matriz de 3 colunas na estrutura tratada do algoritmo TFA.

--- tc/prop_lr.py
+++ tc/prop_lr.py
@@ -1,4 +1,4 @@
-import tc.afd
+import tc.afd as afd
 
 # Recebe um AFD
 # Olhe o arquivo unit.py para ter exemplos de entrada e saída da função
@@ -6,79 +6,67 @@ def tfa_table(automato):
     estados = list(automato[0])
     estados.sort()
         
-    table = []
+    table = {}
 
     for i, estado1 in enumerate(estados[:-1]):
         for estado2 in estados[i+1:]:
-            table.append([estado1, estado2, ''])
+            table[(estado1, estado2)] = ''
     
     return table
 
 def tfa_fill(automato):
-    table = prop_lr.tfa_table(automato)
-    table_aux = []
+    table = tfa_table(automato)
+    table_aux = {}
 
-    for pair in table:
+    for pair in list(table):
         # Se apenas um dos dois estados do par for final, o par é distinguivel
         if (pair[0] in automato[4]) != (pair[1] in automato[4]):
-            pair[2] = 'd'
+            table[pair] = 'd'
         else:
-            table_aux.append(pair)
+            table_aux[pair] = table[pair]
     
     achou_novos = True
 
     while achou_novos:
         # Caso não encontre nenhum par novo que seja distinguivel, o algoritmo chegou ao fim
         achou_novos = False
-        for pair in table_aux:
+        for pair in list(table_aux):
             for simbolo in automato[1]:
-                r = s = []
+                r = s = {}
                 try:
                     r = afd.delta(automato, pair[0], simbolo)
                     s = afd.delta(automato, pair[1], simbolo)
                 except KeyError:
                     continue
-
-                try:
-                    table.index([r, s, 'd'])
-                except ValueError:
-                    pass
-                else:
-                    pair[2] = 'd'
-                    try:
-                        table_aux.remove(pair)
-                    except:
-                        pass
-                    achou_novos = True
                 
-                try:
-                    table.index([s, r, 'd'])
-                except ValueError:
-                    pass
+                pair_aux = None
+                if (r, s) in table:
+                    pair_aux = (r, s)
+                elif (s, r) in table:
+                    pair_aux = (s, r)
                 else:
-                    pair[2] = 'd'
-                    try:
-                        table_aux.remove(pair)
-                    except:
-                        pass
+                    continue
+
+                if table[pair_aux] == 'd':
+                    table[pair] = 'd'
+                    if pair in table_aux:
+                        del table_aux[pair]
                     achou_novos = True
 
     # Para cada par que não for distinguivel, marcá-lo como indistinguivel
-    for pair in table:
-        if pair[2] != 'd':
-            pair[2] = 'i'
+    for pair in list(table):
+        if table[pair] != 'd':
+            table[pair] = 'i'
 
     return table
 
 def tfa(automato):
-    
-    t = tfa_table(automato)
-    table = tfa_fill(automato,t)
+    table = tfa_fill(automato)
     y = []
     
-    for x in table:
-        if(x[2] == ''):
-            y.append([x[0],x[1]])
+    for x in list(table):
+        if table[x] == 'i':
+            y.append(x)
 
     eq = set(tuple(i) for i in y)
     return eq
